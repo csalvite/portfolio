b8e74b1eebff701ccc373be50d8187b370efb698
César Alvite <csalvite@gmail.com>
2024-08-17 17:15:43 +0200

    estructura de pagina de contacto

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -11,6 +11,27 @@ export default function Home() {
       <section className='flex items-center justify-center p-4 rounded-md'>
         <Map />
       </section>
+
+      <section className='flex flex-col gap-8 justify-center p-8 rounded-md'>
+        <div>
+          <h4 className='text-lg mr-4'>Teléfono de contacto: </h4>
+          <p className='text-cyan-300'>+34 633 70 32 01</p>
+        </div>
+        <div>
+          <h4 className='text-lg mr-4'>Dirección: </h4>
+          <p className='text-cyan-300'>
+            Avenida del Rocío, N4, Serra de Outes, A Coruña
+          </p>
+        </div>
+        <div>
+          <h4 className='text-lg mr-4'>Email: </h4>
+          <p className='text-cyan-300'>csalvite@gmail.com</p>
+        </div>
+        <p>
+          Puede ponerse en contacto conmigo para trabajos autónomos y demás e
+          que ainda non sei que carallo poñer aqui xd.
+        </p>
+      </section>
     </div>
   );
 }
