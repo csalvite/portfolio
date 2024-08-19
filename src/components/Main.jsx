a533ffd8a41f0183fd62005862dd9a3baf40e6f5
César Alvite <csalvite@gmail.com>
2024-08-19 16:19:29 +0200

    pruebas de responsive design para ocultar aside

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,41 +1,73 @@
 'use client';
 import Image from 'next/image';
 import Menu from './Menu';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const Main = ({ children }) => {
-  //   const [showAside, setShowAside] = useState(true);
-  //   const lastScrollTop = useRef(0); // Utilizamos useRef para almacenar lastScrollTop
+  const [showAside, setShowAside] = useState(true);
+  const [isMobile, setIsMobile] = useState(false);
+  const lastScrollTop = useRef(0);
+  const sectionRef = useRef(null); // Referencia al section
+  const asideRef = useRef(null); // Referencia al aside
 
-  //   useEffect(() => {
-  //     const handleScroll = () => {
-  //       const scrollTop =
-  //         window.pageYOffset || document.documentElement.scrollTop;
+  // Hook para detectar el tamaño de la ventana
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 1000);
+    };
 
-  //       if (window.innerWidth < 1024) {
-  //         // Aplica solo en dispositivos móviles
-  //         if (scrollTop > lastScrollTop.current) {
-  //           // Desplazamiento hacia abajo, ocultar aside
-  //           setShowAside(false);
-  //         } else {
-  //           // Desplazamiento hacia arriba, mostrar aside
-  //           setShowAside(true);
-  //         }
-  //       }
-  //       lastScrollTop.current = scrollTop <= 0 ? 0 : scrollTop; // Actualiza lastScrollTop
-  //     };
+    // Inicializar el tamaño
+    handleResize();
 
-  //     window.addEventListener('scroll', handleScroll);
+    // Añadir el listener para redimensionar
+    window.addEventListener('resize', handleResize);
 
-  //     return () => window.removeEventListener('scroll', handleScroll);
-  //   }, []);
+    // Limpiar el listener
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (sectionRef.current && isMobile) {
+        const scrollTop = sectionRef.current.scrollTop;
+
+        if (scrollTop > lastScrollTop.current) {
+          // Desplazamiento hacia abajo, ocultar aside
+          setShowAside(false);
+        } else {
+          // Desplazamiento hacia arriba, mostrar aside
+          setShowAside(true);
+        }
+
+        lastScrollTop.current = scrollTop <= 0 ? 0 : scrollTop; // Actualiza lastScrollTop
+      }
+    };
+
+    const sectionElement = sectionRef.current;
+    if (sectionElement) {
+      sectionElement.addEventListener('scroll', handleScroll);
+    }
+
+    return () => {
+      if (sectionElement) {
+        sectionElement.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, [isMobile]); // Dependencia en isMobile para actualizar el scroll listener
 
   return (
     <main className='flex flex-col lg:flex-row gap-4 w-full h-full p-0 md:p-4 lg:p-16 text-[#f5f5f5]'>
       <aside
-        className={`bg-[#353839] flex flex-col xl:w-96 w-full items-center justify-between p-4 rounded-md border-0 md:border-2 border-black transition-transform duration-300`}
+        ref={asideRef}
+        className={`bg-[#353839] flex flex-col xl:w-96 w-full items-center justify-between p-4 rounded-md border border-black transition-transform duration-300 ${
+          isMobile
+            ? showAside
+              ? 'fixed'
+              : 'fixed -translate-y-full'
+            : 'relative'
+        } top-0 left-0 right-0 z-10`}
       >
         <div className='flex flex-col items-center justify-center gap-4'>
           <Image
@@ -68,7 +100,12 @@ const Main = ({ children }) => {
         </nav>
       </aside>
 
-      <section className='bg-[#353839] w-full rounded-md p-2 md:p-8 border-0 md:border-2 border-black overflow-y-scroll'>
+      <section
+        ref={sectionRef}
+        className={`bg-[#353839] w-full flex-1 rounded-md p-2 md:p-8 border-0 md:border-2 border-black overflow-y-scroll ${
+          isMobile ? 'pt-96' : ''
+        }`}
+      >
         {children}
       </section>
     </main>
